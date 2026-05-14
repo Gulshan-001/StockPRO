3ca3d4160860975ce715252a0772372dca73a57a
Gulshan-001 <gulshanthakur1010@gmail.com>
2026-05-14 12:40:56 +0530

    [Gulshan] DEPLOYMENT CHANGES

--- StockPro.WarehouseStock/Data/WarehouseDbContext.cs
+++ StockPro.WarehouseStock/Data/WarehouseDbContext.cs
@@ -12,12 +12,6 @@ namespace StockPro.WarehouseStock.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            
-            // PostgreSQL compatibility: force lowercase table names
-            modelBuilder.Entity<Warehouse>().ToTable("warehouses");
-
-            // Product check is removed here because Product is in another database.
-            // Referential integrity for ProductId is now logical, not physical.
         }
     }
 }
